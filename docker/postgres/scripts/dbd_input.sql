2010fccf9de1d3e4e4b35bdb08098458121d5ff7
tysker <tyskerjoerg@gmail.com>
2021-05-27 14:11:59 +0200

    created input values for database

--- docker/postgres/scripts/dbd_input.sql
+++ docker/postgres/scripts/dbd_input.sql
@@ -1,30 +1,33 @@
--- QUERY FOR KEYWORDS TABLE
-select * from keywords;
+call add_keyword('keyword1');
+call add_keyword('keyword2');
+call add_keyword('keyword3');
 
-select * from log_keywords;
-
-call add_keyword('Hej1');
-
--- QUERY FOR STOCKS TABLE
-select * from stocks;
-
-select * from log_stocks;
-
-call add_stock('Stock1');
-
--- QUERY FOR USERS TABLE
-select * from users;
-
-select * from log_users;
+call add_stock('stock1');
+call add_stock('stock2');
+call add_stock('stock3');
 
 call add_user('username1', 'pwd1', 'email1');
+call add_user('username2', 'pwd2', 'email2');
+call add_user('username3', 'pwd3', 'email3');
 
-select * from view_users;
 
--- QUERY FOR USERS_KEYWORDS TABLE
 insert into users_keywords values (1,1);
-select * from view_users_keywords;
+insert into users_keywords values (1,2);
+insert into users_keywords values (1,3);
 
--- QUERY FOR USERS_STOCKS TABLE
+insert into users_keywords values (2,1);
+insert into users_keywords values (2,2);
+insert into users_keywords values (2,3);
+
+
+insert into users_keywords values (3,2);
+insert into users_keywords values (3,3);
+
+
 insert into users_stocks values (1,1);
-select * from view_users_stocks;
+insert into users_stocks values (1,2);
+
+insert into users_stocks values (2,2);
+insert into users_stocks values (2,3);
+
+insert into users_stocks values (3,3);
